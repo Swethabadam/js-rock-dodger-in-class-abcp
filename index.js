997f3cdc7fc87089e5961b90d8f1670fc131bf71
SWETHA BADAM <sweth.badam@gmail.com>
2018-01-21 18:31:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,7 +74,17 @@ function endGame() {
   return alert('YOU LOSE!')
 }
 
+function moveDodger(e) {
+  const code = e.which
 
+  if ([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+
+  if (code === LEFT_ARROW) {
+    moveDodgerLeft()
+  } else if (code === RIGHT_ARROW) {
     moveDodgerRight()
   }
 }
